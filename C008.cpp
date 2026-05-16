7e23eda4587325095e445ec9ad1d8169efdbaad4
nguyendat1101 <Nguyenminhdat160407@gmail.com>
2026-05-16 15:36:46 +0700

    Code C

--- C008.cpp
+++ C008.cpp
@@ -9,7 +9,7 @@ typedef TNODE* TREE;
 
 void NLR (TREE Root){
 	if (Root != NULL){
-		printf("%d", Root->data);
+		printf("%d\t", Root->data);
 		NLR(Root->pLeft);
 		NLR(Root->pRight);
 	}
@@ -18,7 +18,7 @@ void NLR (TREE Root){
 void LNR (TREE Root){
 	if(Root != NULL){
 		LNR(Root->pLeft);
-		printf("%d", Root->data);
+		printf("%d\t", Root->data);
 		LNR(Root -> pRight);
 	}
 }
@@ -27,7 +27,7 @@ void LRN (TREE Root){
 	if (Root != NULL){
 		LRN(Root->pLeft);
 		LRN(Root->pRight);
-		printf("%d", Root->data);
+		printf("%d\t", Root->data);
 	}
 }
 
@@ -47,16 +47,46 @@ void ThemNode(TREE* root, int x){
 		if (x < (*root)->data) ThemNode(&((*root)->pLeft), x);
 		else if (x > (*root)->data) ThemNode (&((*root)->pRight), x);
 	}
-} 
+}
+
+void CountNodes(TREE root, int* x){
+	if (root != NULL){
+		(*x)++;
+		CountNodes(root->pLeft, x);
+		CountNodes(root->pRight, x);
+	}
+}
+
+int max(int a, int b){
+	if (a>b) return a;
+	return b;
+}
+
+int TreeHeight(TREE Root){
+	if (Root == NULL){
+		return -1;
+	}
+	int dosaubentrai = TreeHeight(Root->pLeft);
+	int dosaubenphai = TreeHeight(Root->pRight);
+	return max(dosaubentrai,  dosaubenphai) + 1;
+}
+
+int CountLeaves(TREE Root, int *x){
+	if (Root != NULL) {
+		if(Root->pLeft == NULL && Root->pRight == NULL) return (*x)++;
+	CountLeaves(Root->pLeft, x);
+	CountLeaves(Root->pRight, x);
+	}
+}
 
 int main(){
 	TREE caynhiphan = NULL;
-	int n, x;
+	int n, x, SoLuongNode=0, SoLuongNodeLa=0;
 	
 	printf("\n nhap so luong Node cho cay nhi phan:");
 		scanf("%d", &n);
 	
-	for(int i=0; i<=n; i++){
+	for(int i=0; i<n; i++){
 		printf("\nNhap gia tri Node thu %d:", i+1);
 			scanf("%d", &x);
 		ThemNode(&caynhiphan, x);
@@ -67,6 +97,11 @@ int main(){
 	LNR(caynhiphan);
 	printf("\n Ket qua duyet cat LRN ");
 	LRN(caynhiphan);
-
+	CountNodes(caynhiphan, &SoLuongNode);
+	printf("\ntong so node trong cay la:%d", SoLuongNode);
+	int docao = TreeHeight(caynhiphan);
+	printf("\ndo cao cua cay nhi phan la:%d", docao);
+	CountLeaves(caynhiphan, &SoLuongNodeLa);
+	printf("\nso nut la trong cay nhi phan la:%d", SoLuongNodeLa);
 return 0;
 }
